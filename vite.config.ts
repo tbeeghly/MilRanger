53337ec0c97471662885c4a1d7525ac83792480d
tbeeghly <taylor.beeghly@gmail.com>
2025-06-04 08:37:42 -0700

    v1.0 release build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MilRanger/', // This should match your GitHub repo name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   server: {
     hmr: true,
     watch: {
